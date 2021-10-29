cb300d26d3b6a0380a2e620fca1461e2c4402bad
Samikshya <write2samikshyakar@gmail.com>
2021-10-29 10:40:50 +0530

    Add project files.

---
+++ WebApplication5/Company.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication5
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+    }
+
+    public class Company
+    {
+        private List<Employee> EmployeeLists;
+        public Company()
+        {
+            EmployeeLists = new List<Employee>();
+            EmployeeLists.Add(new Employee() { ID = 1, Name = "Amu", Gender = "M" });
+            EmployeeLists.Add(new Employee() { ID = 2, Name = "Samu", Gender = "F" });
+            EmployeeLists.Add(new Employee() { ID = 3, Name = "Amu", Gender = "M" });
+            EmployeeLists.Add(new Employee() { ID = 4, Name = "Samu", Gender = "F" });
+            EmployeeLists.Add(new Employee() { ID = 5, Name = "Amu", Gender = "M" });
+            EmployeeLists.Add(new Employee() { ID = 6, Name = "Samu", Gender = "F" });
+
+
+        }
+
+        public string this[int empID]
+        {
+            get { return EmployeeLists.FirstOrDefault(emp => emp.ID == empID).Name; }
+
+            set { EmployeeLists.FirstOrDefault(emp => emp.ID == empID).Name = value; }
+
+        }
+
+
+
+
+    }
+}
---
+++ WebApplication5/WebForm1.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication5
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Company company = new Company();
+            Response.Write("Name of emp with ID = 2" + "::::::" +  company[2]);
+            Response.Write("<br/>");
+            Response.Write("<br/>");
+            Response.Write("Employee Names changed ");
+            company[2] = "2nd employee name changed ";
+            Response.Write("<br/>");
+            Response.Write("<br/>");
+            Response.Write("Name of emp with ID = 2" + "::::::" + company[2]);
+
+        }
+    }
+}
